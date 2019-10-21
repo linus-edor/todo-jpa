362ca641bf804fc834b80e07b1c8f788735f3495
Linus <edorlinus@gmail.com>
2019-10-21 11:41:55 +0000

    initial commit

---
+++ src/test/java/com/boot/todo/jpa/TodoJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boot.todo.jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
